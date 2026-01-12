58eb18f3aea5cf553e365aef2d7ce13c83aef3b1
sdrangan <sdrangan@gmail.com>
2026-01-11 23:40:40 -0500

    Removed argparse from main to enable operation in render.com

--- run.py
+++ run.py
@@ -3,10 +3,7 @@ from llmgrader.app import create_app
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--questions_repo", type=str, default=None,\
-                    help="Git repo URL for questions (if not local)")
-args = parser.parse_args()
+
 questions_root = os.path.join(os.getcwd(), "questions")
 scratch_dir = os.path.join(os.getcwd(), "scratch")
 
@@ -15,6 +12,11 @@ app = create_app(
     scratch_dir=scratch_dir)
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--questions_repo", type=str, default=None,\
+                        help="Git repo URL for questions (if not local)")
+    args = parser.parse_args()
     
     # Run the app
     # Note: use_reloader=False to avoid double initialization of Grader
