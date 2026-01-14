bbfa2f8c912d7d1255d38b29737b219e4a13037b
sdrangan <sdrangan@gmail.com>
2026-01-14 08:39:43 -0500

    Corrected calling to run on render

--- run.py
+++ run.py
@@ -8,23 +8,28 @@ qrepo = None
 questions_root = os.path.join(os.getcwd(), "questions")
 scratch_dir = os.path.join(os.getcwd(), "scratch")
 
-
-# Called in command line (not called when running in render.com)
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--qrepo", type=str, default=None,\
+    parser.add_argument("--qrepo", type=str, default=None,
                         help="Git repo URL for questions (if not local)")
     args = parser.parse_args()
     qrepo = args.qrepo
 
+    # Create the Flask app
+    app = create_app(
+        questions_root=questions_root,
+        scratch_dir=scratch_dir,
+        qrepo=qrepo
+    )
 
-# Create the Flask app
-app = create_app(
-    questions_root=questions_root, 
-    scratch_dir=scratch_dir,
-    qrepo=qrepo)
-    
-# Run the app
-# Note: use_reloader=False to avoid double initialization of Grader
-app.run(debug=True, use_reloader=False) 
+    # Run locally only
+    app.run(debug=True, use_reloader=False)
+
+else:
+    # Render imports this branch
+    app = create_app(
+        questions_root=questions_root,
+        scratch_dir=scratch_dir,
+        qrepo=qrepo
+    )
